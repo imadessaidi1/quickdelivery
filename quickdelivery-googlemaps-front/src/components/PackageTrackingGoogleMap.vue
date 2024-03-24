d115315310691f468783bf852eb2db72bba609d8
Imad ESSAIDI <im.essaidi@gmail.com>
2024-03-24 15:35:40 +0100

    Tracking with package details

--- quickdelivery-googlemaps-front/src/components/PackageTrackingGoogleMap.vue
+++ quickdelivery-googlemaps-front/src/components/PackageTrackingGoogleMap.vue
@@ -1,25 +1,43 @@
 <template>
-  <div class="google-map">
-    <div id="map">
-    <iframe
-            ref="map"
-            width="100%"
-          height="100%"
-          :src="googleMapPath"
-          style="border:0;"
-          @load="onLoadIframe"
-          name="map"
-        ></iframe>
+    <div class="d-flex">
+        <div class="summary_component" style="width: 30%;">
+            <PackageSummary/>
+        </div>
+        <div class="google-map" style="width: 70%;">
+            <div id="map">
+                <iframe
+                        ref="map"
+                        width="100%"
+                        height="100%"
+                        :src="googleMapPath"
+                        style="border: 0;"
+                @load="onLoadIframe"
+                name="map"
+                ></iframe>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
+import PackageSummary from '../components/PackageDetails.vue';
+import http from '@/config/httpInterceptor';
 export default {
+  components: {
+      PackageSummary,
+  },
   data() {
       return {
         googleMapPath: process.env.BASE_URL + 'google-maps-package-tracking.html',
       };
+  },
+  mounted() {
+    http.get(this.$i18n.t('rootURL') + this.$i18n.t('getPackage')+this.$route.params.packageReference)
+      .then(response => {
+        this.$store.commit('updatePackage', response.data);
+    }).catch(() => {
+      console.log("unable to process your request this time. please try again latter.");
+    });
   },
   methods: {
     onLoadIframe() {

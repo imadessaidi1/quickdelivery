5e5736f457d8e757622a76515218c6199319b099
Oualid ESSAIDI <oualid@MacBook-Pro-de-Oualid.local>
2024-03-24 11:58:55 +0000

    Carousel fixed

--- quickdelivery-googlemaps-front/src/pages/HomePage.vue
+++ quickdelivery-googlemaps-front/src/pages/HomePage.vue
@@ -23,10 +23,10 @@ export default {
 <style>
 .carousel-overlay {
   position: absolute;
-  top: 58px;
+  top: 7%;
   left: 0;
   width: 100%; /* Couvre toute la largeur de la carte */
-  height: 31%; /* Ajustez la hauteur selon vos besoins */
+  height: 270px; /* Ajustez la hauteur selon vos besoins */
   background: rgba(255, 255, 255, 0.3); /* Couleur de fond avec transparence */
   z-index: 1; /* Mettez un z-index plus élevé que la carte pour le superposer */
   overflow: hidden; /* Empêche le contenu de déborder */
